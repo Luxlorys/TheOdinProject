879806f52871faa1aefb028f9a243f3c40edf0ab
luxlorys <bossbyrcev221@gmail.com>
2023-05-10 14:11:01 +0300

    created enetities

--- task2/js/index.js
+++ task2/js/index.js
@@ -1,9 +1,22 @@
+// two entities: player_x and player_o
+// all win combinations
+
 window.addEventListener('DOMContentLoaded', () => {
     const cells = document.querySelectorAll('.cell');
 
+    // entities 
+    function playerBuilder(mark, turn) {
+        return {mark, turn};
+    };
+
+    const player_x = playerBuilder("x", true);
+    const player_o = playerBuilder("o", false)
+
     cells.forEach(cell => {
-        cell.addEventListener('click', () => {
+        cell.addEventListener('click', (it) => {
         console.log(`Cell ${cell.textContent} clicked!`);
+        cell.classList.add(it.target.textContent = 'clicked')
         });
     });
 })
+

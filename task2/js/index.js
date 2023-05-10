b76d4f456117f17687de5396b9d7a4a796fca5d8
luxlorys <bossbyrcev221@gmail.com>
2023-05-10 15:04:11 +0300

    simple game logic was created

--- task2/js/index.js
+++ task2/js/index.js
@@ -2,7 +2,7 @@
 // all win combinations
 
 window.addEventListener('DOMContentLoaded', () => {
-    const cells = document.querySelectorAll('.cell');
+    let board = []
 
     // entities 
     function playerBuilder(mark, turn) {
@@ -10,13 +10,29 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     const player_x = playerBuilder("x", true);
-    const player_o = playerBuilder("o", false)
+    const player_o = playerBuilder("o", false);
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', (it) => {
-        console.log(`Cell ${cell.textContent} clicked!`);
-        cell.classList.add(it.target.textContent = 'clicked')
-        });
-    });
+    
+
+    const playTurn = (function() {
+        const cells = document.querySelectorAll('.cell');
+
+        cells.forEach(cell => {
+            cell.addEventListener('click', (event) => {
+
+                if(player_x.turn && cell.textContent === '') {
+                    cell.textContent = player_x.mark;
+                    
+                    player_x.turn = false;
+                    player_o.turn = true;
+                } else if (player_o.turn && cell.textContent === '') {
+                    cell.textContent = player_o.mark;
+
+                    player_o.turn = false;
+                    player_x.turn = true;
+                }
+            }); // cells event listener
+        }) // forEach
+    })(); // playTurn
 })
 

1e09d25b1892c2c1f7a3c58106f55a2ef5ee4788
luxlorys <bossbyrcev221@gmail.com>
2023-06-28 19:54:13 +0300

    still learning

--- nodejsProjects/2/index.js
+++ nodejsProjects/2/index.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const path = require("path");
+
 const members = require("./Members");
-const moment = require("moment");
+const logger = require('./middleware/logger')
 
 const app = express();
 const port = 3000;
-
-const logger = (req, res, next) => {
-  console.log(
-    `${req.protocol}://${req.get("host")}${
-      req.originalUrl
-    }: ${moment().format()}`
-  );
-  next();
-};
 
 app.use(logger);
 
@@ -21,6 +13,17 @@ app.use(logger);
 app.get("/api/v1/members", (req, res) => {
   res.json(members);
 });
+
+// get single member
+app.get('/api/v1/members/:id', (req, res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id))
+
+    if (found) {
+        res.json(members.filter(member => member.id === parseInt(req.params.id)))
+    } else {
+        res.status(400).json({ msg: `no members with ${req.params.id} id` });
+    }
+})
 
 app.listen(port, (err, response) => {
   console.log(`port: ${port}`);
